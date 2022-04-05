360f22e1882c88632dc523111f59936525924405
Akshay Sayre <73643584+im4kshay@users.noreply.github.com>
2022-04-05 12:34:56 +0530

    Update PrimeFactorization.cs

--- BasicCorePrograms/PrimeFactorization.cs
+++ BasicCorePrograms/PrimeFactorization.cs
@@ -8,16 +8,17 @@ namespace BasicCorePrograms
 {
     class PrimeFactorization
     {
-        public int j = 0;
-        public int num = 0;
-        public int factorial = 1;
+        int a, b;
         public void CalcSFactorialSeries()
         {
-            Console.Write("Calculate the factorial of a given number:"); //Input From User
-            num = Convert.ToInt32(Console.ReadLine());//reading input form user
-            for (j = 1; j <= num; j++) //Checking var_j is <= num example(1<5) and den incrementing it by 1 
-                factorial *= j; //calculate  factorial = factorial * var_j example (factroial = 1*1 or 2*2 or 5*5)
-            Console.Write("The Factorial of {0} is: {1}\n", num, factorial); //op
-        }
+            Console.WriteLine("Please enter your integer: ");
+            a = int.Parse(Console.ReadLine());
+            for (b = 1; b <= a; b++)
+            {
+                if (a % b == 0)
+                {
+                Console.WriteLine(b + " is a factor of " + a);
+                }
+            }
     }
 }
